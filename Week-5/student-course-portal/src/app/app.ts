2160b572c599dd7d7b2962f4dc821e71413be478
Mahima Kumari <mahimakri425@gmail.com>
2026-07-21 20:02:33 +0530

    Completed Week-5 Angular Hands-On 2

--- Week-5/student-course-portal/src/app/app.ts
+++ Week-5/student-course-portal/src/app/app.ts
@@ -6,6 +6,7 @@ import { Home } from './pages/home/home';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, Header, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
--- Week-5/student-course-portal/src/app/pages/home/home.ts
+++ Week-5/student-course-portal/src/app/pages/home/home.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home {}
+export class Home implements OnInit {
+
+  studentName = 'Mahima Kumari';
+  department = 'ECE';
+  cgpa = 8.5;
+  isEnrolled = true;
+  courseName = '';
+
+  ngOnInit(): void {
+    console.log('Home Component Loaded Successfully!');
+  }
+
+  showMessage() {
+    alert('Welcome to Student Course Portal!');
+  }
+}
