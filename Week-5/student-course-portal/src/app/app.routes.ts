32748b81a2b067d2ef20ccb54faa475a55044ddf
Mahima Kumari <mahimakri425@gmail.com>
2026-07-22 23:24:04 +0530

    Completed Angular Hands-On 4

--- Week-5/student-course-portal/src/app/app.routes.ts
+++ Week-5/student-course-portal/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+
 import { Home } from './pages/home/home';
 import { CourseList } from './pages/course-list/course-list';
+import { EnrollmentForm } from './pages/enrollment-form/enrollment-form';
 
 export const routes: Routes = [
   {
@@ -10,5 +12,9 @@ export const routes: Routes = [
   {
     path: 'courses',
     component: CourseList
+  },
+  {
+    path: 'enroll',
+    component: EnrollmentForm
   }
 ];
