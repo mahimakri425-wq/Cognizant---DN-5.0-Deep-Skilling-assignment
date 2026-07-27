fb2be414a70784b901a4b467b306f0924413c8bc
Mahima Kumari <mahimakri425@gmail.com>
2026-07-27 23:55:42 +0530

    Complete React Hands-on 2

---
+++ Week-6/React/HandsOn-2/studentapp/src/Components/Contact.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class Contact extends Component {
+  render() {
+    return (
+      <h3>Welcome to the Contact page of the Student Management Portal</h3>
+    );
+  }
+}
